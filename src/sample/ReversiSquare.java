866235c80fa6c23e61af99b582c1c54e337da1eb
Sam Bokai <onlineshrekjr@yahoo.com>
2016-11-18 12:51:55 +0100

    - add settings pane - add radiobuttons (Difficulty and Gridsize) - group up radiobuttons in a togglegroup

---
+++ src/sample/ReversiSquare.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.scene.layout.Region;
+
+/**
+ * Created by sambokai on 18.11.16.
+ */
+public class ReversiSquare extends Region{
+    public ReversiSquare() {
+        setStyle("-fx-background-color: darkolivegreen");
+        setPrefSize(200,200);
+    }
+}
